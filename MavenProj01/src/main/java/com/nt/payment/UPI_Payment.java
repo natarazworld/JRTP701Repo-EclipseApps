639a1b362abf5603760f0caf456a1f12337b126f
nataraz <natarazjavaarena@gmail.com>
2024-05-02 08:08:33 +0530

    TL - createed  Payment Branch and added UPI_Payment class

---
+++ MavenProj01/src/main/java/com/nt/payment/UPI_Payment.java
@@ -0,0 +1,8 @@
+package com.nt.payment;
+
+public class UPI_Payment {
+     public String doPayment(long phoneno, double amount) {
+    	 System.out.println("Payment is done");
+    	 return  "Payment is done";
+     }
+}
