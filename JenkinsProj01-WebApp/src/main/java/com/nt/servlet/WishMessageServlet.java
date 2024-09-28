89caa7e0be9634fb28c5569de2c18da69baec08b
nataraz <natarazjavaarena@gmail.com>
2024-09-28 07:20:38 +0530

    change1 committed

--- JenkinsProj01-WebApp/src/main/java/com/nt/servlet/WishMessageServlet.java
+++ JenkinsProj01-WebApp/src/main/java/com/nt/servlet/WishMessageServlet.java
@@ -21,7 +21,7 @@ public class WishMessageServlet extends HttpServlet {
 		  // get PrintWriter
 		   PrintWriter pw=resp.getWriter();
 		   // write  the messages to  response object
-		   pw.println("<h1 style='color:red;text-align:center'> Good Morning  </h1>");
+		   pw.println("<h1 style='color:red;text-align:center'>Hello, Good Morning  </h1>");
 		   //home hyperlink
 		   pw.println("<br><br><a href='index.jsp'> home </a>");
 		  //close stream
--- JenkinsProj01-WebApp/src/main/webapp/index.jsp
+++ JenkinsProj01-WebApp/src/main/webapp/index.jsp
@@ -1,6 +1,6 @@
 
 
 
-<h1 style="color:red;text-align">  Jenkins  web application  </h1>
+<h1 style="color:red;text-align">  Jenkins  web application 123  </h1>
 <br><br>
 <a  href="wishurl">  show wish message</a>
