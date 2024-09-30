3b77b67967727f2f5956ce80ae9feba3f2c70a9c
nataraz <natarazjavaarena@gmail.com>
2024-09-30 07:36:45 +0530

    jenkins rest api --initial commit

---
+++ JenikinsProj02-RestAPI/src/main/java/com/nt/JenikinsProj02RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenikinsProj02RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenikinsProj02RestApiApplication.class, args);
+	}
+
+}
---
+++ JenikinsProj02-RestAPI/src/main/java/com/nt/rest/WishMessageOperationsController.java
@@ -0,0 +1,18 @@
+package com.nt.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wish-api")
+public class WishMessageOperationsController {
+	
+	@GetMapping("/display")
+	public   ResponseEntity<String>  showWishMessage(){
+		return  new ResponseEntity<String>("Good Morning  JTRP ",HttpStatus.OK);
+	}
+
+}
